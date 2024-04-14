590fb94006b4caee3f0af4df2495ec07856f9c4d
pavelzhelev1 <114466874+pavelzhelev1@users.noreply.github.com>
2024-04-14 11:28:05 +0300

    Delete _Car_Rent directory

--- _Car_Rent/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace _Car_Rent.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
